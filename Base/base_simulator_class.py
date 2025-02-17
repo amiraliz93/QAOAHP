c00ef47c44df0c7fc2453e7288dce390b1807c0a
N1259534 <N1259534@my.ntu.ac.uk>
2025-02-17 16:26:38 +0000

    1

--- Base/base_simulator_class.py
+++ Base/base_simulator_class.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+#Allows you to reference classes and functions that are defined later in the code.
+# which means that type hints can be written as strings and will be evaluated only when needed
 import typing
 import numpy as np
 from abc import ABC, abstractmethod
