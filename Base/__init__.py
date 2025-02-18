24bb69dfd91fc4a5e5b834e0293dea2e115e18b9
N1259534 <N1259534@my.ntu.ac.uk>
2025-02-18 11:04:39 +0000

    manage file

--- Base/__init__.py
+++ Base/__init__.py
@@ -1,2 +1,61 @@
-from .QAOA_objective import get_qaoa_objective
-from .Simulators import 
+
+import numba.cuda
+from .qaoa_simulator_base import QAOASimulationBase
+from ..Simulators.python import QAOAFURXSimulator, ParamType, CostsType, TermsType
+
+Simulators = {
+    "x": {
+       # "c": QAOAFURXSimulatorC,
+        "python": QAOAFURXSimulator,
+        #"gpu": QAOAFURXSimulatorGPU,
+        #"gpumpi": QAOAFURXSimulatorGPUMPI,
+    }
+}
+
+def get_available_simulator_names(type: str = "x") -> list:
+    """_summary_
+
+    Args:
+        type (str, optional): name of simulator. Defaults to "x".
+
+    Returns:
+        list: available simulator
+    """
+    family = Simulators.get(type, None)
+    if family is None:
+        raise ValueError(f'the simulator is not defined {type}')
+
+    precedence = ["python", "gpumpi", "gpu", "c" ]
+
+    check = [mpi_available, numba.cuda.is_available, c_available]
+    available = []
+    for i in range(len(check)):
+        if precedence[i] not in family:
+            continue
+        if checks[i]():
+            available.append(precedence[i])
+    
+    available.append(precedence[-1])
+    return available
+
+
+def get_available_simulators(type: str = "x") -> list:
+    """
+    Return (uninitialized) classes of available simulators
+
+    Parameters
+    ----------
+        type: type of QAOA mixer to simulate
+
+    Returns
+    -------
+        List of available simulators
+    """
+    available_names = get_available_simulator_names(type=type)
+    return [Simulators[type][s] for s in available_names]
+
+
+def choose_simulator(name="auto", **kwargs):
+    if name != "auto":
+        return Simulators["x"][name]
+    return Simulators["x"]['python']
--- Simulators/python/QAOA_simulator.py
+++ Simulators/python/QAOA_simulator.py
@@ -3,9 +3,96 @@ from __future__ import annotations
 # which means that type hints can be written as strings and will be evaluated only when needed
 from collections.abc import Sequence
 import numpy as np
-from Base import QAOAFastSimulatorBase, CostsType, TermsType, ParamType
+from QAOA_mtrix_simulation.Base.qaoa_simulator_base import QAOASimulationBase, CostsType, TermsType, ParamType
+from ...Base.qaoa_simulator_base import QAOASimulationBase, CostsType, TermsType, ParamType
 from ...precoputation.numpy_vectorized import precompute_vectorized_cpu_parallel
-from .QAOA_simulator import apply_qaoa_furx
+from ...Simulators.python.QAOA_rotation import apply_qaoa_furx
 
 
+
+class QAOAFastSimulatorPythonBase(qaoa_simulator_base):
+    _hc_diag: np.ndarray
+
+    def _diag_from_costs(self, costs: CostsType):
+        return np.asarray(costs, dtype="float")
+
+    def _diag_from_terms(self, terms: TermsType):
+        a = precompute_vectorized_cpu_parallel(terms, 0.0, self.n_qubits)
+        return a
+
+    def get_cost_diagonal(self) -> np.ndarray:
+        return self._hc_diag
+
+    @property
+    def default_sv0(self):
+        return np.full(self.n_states, 1.0 / np.sqrt(self.n_states), dtype="complex")
+
+    def _apply_qaoa(self, sv: np.ndarray, gammas: Sequence[float], betas: Sequence[float], **kwargs):
+        raise NotImplementedError
+    
+     # -- Outputs
+    def get_statevector(self, result: np.ndarray, **kwargs) -> np.ndarray:
+        return result
+
+    def get_probabilities(self, result: np.ndarray, **kwargs) -> np.ndarray:
+        return np.abs(result) ** 2
+   
+    def get_expectation(self, result: np.ndarray, costs: np.ndarray | None = None, optimization_type="min", **kwargs) -> float:
+        if costs is None:
+            costs = self._hc_diag
+        if optimization_type == "max":
+            return -1 * np.dot(costs, np.abs(result) ** 2)
+        return np.dot(costs, np.abs(result) ** 2)
+    
+
+    def simulate_qaoa(
+        self,
+        gammas: ParamType,
+        betas: ParamType,
+        sv0: np.ndarray | None = None,
+        **kwargs,
+    ) -> np.ndarray:
+        """
+        simulator QAOA circuit using FUR
+        @param gammas parameters for the phase separating layers
+        @param betas parameters for the mixing layers
+        @param sv0 (optional) initial statevector, default is uniform superposition state
+        @return statevector or vector of probabilities
+        """
+        sv = sv0.astype("complex") if sv0 is not None else self.default_sv0
+        self._apply_qaoa(sv, list(gammas), list(betas), **kwargs)
+        return sv
+
+    def get_overlap(
+        self, result: np.ndarray, costs: CostsType | None = None, indices: np.ndarray | Sequence[int] | None = None, optimization_type="min", **kwargs
+    ) -> float:
+        """
+        Compute the overlap between the statevector and the ground state
+
+        Parameters
+        ----------
+            result: statevector
+            costs: (optional) diagonal of the cost Hamiltonian
+            indices: (optional) indices of the ground state in the statevector
+            preserve_state: (optional) if True, allocate a new array for probabilities
+        """
+        probs = self.get_probabilities(result, **kwargs)
+        if indices is None:
+            if costs is None:
+                costs = self._hc_diag
+            else:
+                costs = self._diag_from_costs(costs)
+            if optimization_type == "max":
+                val = costs.max()
+            else:
+                val = costs.min()
+            indices = (costs == val).nonzero()
+        return probs[indices].sum()
+    
+    
+class QAOAFURXSimulator(QAOAFastSimulatorPythonBase):
+    def _apply_qaoa(self, sv: np.ndarray, gammas: Sequence[float], betas: Sequence[float], **kwargs):
+        apply_qaoa_furx(sv, gammas, betas, self._hc_diag, self.n_qubits)
+        # the _apply_qaoa is abstract method and subclass must provided
+        # apply_qaoa_furx is implemented as method for _apply_qaoa
 
