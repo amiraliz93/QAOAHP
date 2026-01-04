a76e99792ce650b909accb50417264d4f7cf5681
hiroki Shibata <hshibata@tmu.ac.jp>
2026-01-04 18:50:44 +0900

    pudate, finish mixer operation, need to be tested although

--- main/Base/Simulators/FPGA/NTU_FPGA2/top1.sv
+++ main/Base/Simulators/FPGA/NTU_FPGA2/top1.sv
@@ -12,7 +12,7 @@ parameter NM = 13; // address width for state vector's and cost function's BRAM.
 parameter P = 64; // data width for numerical number
 parameter Ni = 32;// data width of auxiary information on pipeline.
 parameter NBRAM = 6; // number of block RAMs connected to qaoa system.
-
+localparam N_BIT_SWAP_POINTER = $clog2(NM);
 // Declare signals to connect to the UART module
 wire  [63:0] r_data;
 wire  [63:0] r_addr;
@@ -68,7 +68,15 @@ wire  [P-1:0]   Hr_o;
 wire  [P-1:0]   Hi_o;
 wire  [Ni-1:0]    info_inGC; // information, like addresses, enabled signal, and so on.
 wire  [Ni-1:0]    info_outGC; // information, like addresses, enabled signal, and so on.
-qaoa_system2 #(.NM(NM), .P(P), .NBRAM(NBRAM), .Ni(Ni)) qs2
+
+
+wire [Ni-1:0] bs_info_out;
+wire [Ni-1:0] bs_info_in;
+wire [NM-1:0] bswap_in;  // bit swap in swap pointer 1
+wire [NM-1:0] bswap_out;
+wire [N_BIT_SWAP_POINTER-1] bsp1; // next bit swap pointer 1
+wire [N_BIT_SWAP_POINTER-1] bsp2; // next bit swap pointer 2
+qaoa_system2 #(.NM(NM), .P(P), .NBRAM(NBRAM), .Ni(Ni) .N_BIT_SWAP_POINTER(N_BIT_SWAP_POINTER)) qs2
 (
    .CLK(CLK),
    .RST(RST),
@@ -102,6 +110,22 @@ qaoa_system2 #(.NM(NM), .P(P), .NBRAM(NBRAM), .Ni(Ni)) qs2
    .Hi_res(Hi_o),
    .info_inGC(info_inGC), // information, like addresses, enabled signal, and so on.
    .info_outGC(info_outGC) // information, like addresses, enabled signal, and so on.
+
+   .bs_info_out(bs_info_out);
+   .bs_info_in(bs_info_in);
+   .bswap_in(bswap_in);  // bit swap in swap pointer 1
+   .bswap_out(bswap_out);
+   .bsp1(bsp1); // next bit swap pointer 1
+   .bsp2(bsp2); // next bit swap pointer 2
+);
+
+bit_swap #(.M(M), .N(N), .Np(Ni), .Ni(N_BIT_SWAP_POINTER)) b0(.CLK(CLK),
+    .a_in(bswap_in),
+    .a_out(bswap_out),
+    .q_in(bsp1),
+    .p_in(bsp2),
+    .info_in(bs_info_in),
+    .info_out(bs_info_out)
 );
 gen_cost  #(.P(P),.Ni(Ni)) genCost
   (
