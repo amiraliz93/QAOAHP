83b0b7d6718225294eae6e70a195239c78ee695b
Hiroki Shibata <hshibata@tmu.ac.jp>
2026-05-04 19:08:12 +0100

    update

--- main/Base/Simulators/FPGA/NTU_FPGA2/mixer2.sv
+++ main/Base/Simulators/FPGA/NTU_FPGA2/mixer2.sv
@@ -111,73 +111,57 @@ integer i;
 localparam [63:0] SIGN_MASK = 64'h8000_0000_0000_0000;
 
 always @(posedge CLK) begin
-	if (RST) begin
-            for (i = 0; i < NPip; i = i + 1) begin
-                  p_info[i] <= 0;
-                  p_switch[i] <= 0;
-            end
-            cb_0Pip <= 1; // 0.5000000000000001
-            sb_0Pip <= 0; // 0.8660254037844386
-            for (i = 0; i < 3; i = i + 1) begin
-                  prc_N1Pip[i] <= '0;
-                  prs_N1Pip[i] <= '0;
-                  pic_N1Pip[i] <= '0;
-                  pis_N1Pip[i] <= '0;
-            end
-	end
-      else begin
-            cb_0Pip <= cb;
-            sb_0Pip <= sb;
-            pr_0Pip <= p_ar;
-            pi_0Pip <= p_ai;
-            p_info[0] <= info_in;
-            p_switch[0] <= switch_in;
-            prc_N1Pip[0] <= n_prc_N1Pip;
-            prs_N1Pip[0] <= n_prs_N1Pip;
-            pic_N1Pip[0] <= n_pic_N1Pip;
-            pis_N1Pip[0] <= n_pis_N1Pip;
-            for (i = 0; i < 2; i = i + 1) begin
-                  prc_N1Pip[i+1] <= prc_N1Pip[i];
-                  prs_N1Pip[i+1] <= prs_N1Pip[i];
-                  pic_N1Pip[i+1] <= pic_N1Pip[i];
-                  pis_N1Pip[i+1] <= pis_N1Pip[i];
-            end
-            for (i = 0; i <= NPip-2; i = i + 1) begin
-                  p_info[i+1]   <= p_info[i];
-                  p_switch[i+1] <= p_switch[i];
-            end
-
-            case(p_switch[N1])
-                  2'b01: begin
-                        // it is assumed in this case, N1Pip[1] has p_a, and N1Pip[0] has p_b. Generate p'_a.
-                        // adder 1 will performe, pp_ar = cosb * p_ar - sinb * p_bi
-                        // adder 2 will performe, pp_ai = cosb * p_ai + sinb * p_br
-                        add1_a <= prc_N1Pip[1];
-                        add1_b <= pis_N1Pip[0] ^ SIGN_MASK;
-                        add2_a <= pic_N1Pip[1];
-                        add2_b <= prs_N1Pip[0];
-                  end
-                  2'b10: begin
-                        // it is assumed in this case, N1Pip[2] has p_a, and N1Pip[1] has p_b. Generate p'_b.
-                        // adder 1 will performe, pp_br = - sinb * p_ai + cosb * p_br
-                        // adder 2 will performe, pp_bi = sinb * p_ar + cosb * p_bi
-                        add1_a <= prc_N1Pip[1];
-                        add1_b <= pis_N1Pip[2] ^ SIGN_MASK;
-                        add2_a <= pic_N1Pip[1];
-                        add2_b <= prs_N1Pip[2];
-                  end
-                  default: begin
-                        // this is for cost function operator
-                        // adder 1 will performe, pp_ar = - sinb* p_ai + cosb *p_ar
-                        // adder 2 will performe, pp_ai = sinb *p_ar + cosb *p_ai
-                        add1_a <= prc_N1Pip[1];
-                        add1_b <= pis_N1Pip[1] ^ SIGN_MASK;
-                        add2_a <= pic_N1Pip[1];
-                        add2_b <= prs_N1Pip[1];
-                  end
-            endcase
-            pr_N3Pip <= add1_res;
-            pi_N3Pip <= add2_res;
+      cb_0Pip <= cb;
+      sb_0Pip <= sb;
+      pr_0Pip <= p_ar;
+      pi_0Pip <= p_ai;
+      p_info[0] <= info_in;
+      p_switch[0] <= switch_in;
+      prc_N1Pip[0] <= n_prc_N1Pip;
+      prs_N1Pip[0] <= n_prs_N1Pip;
+      pic_N1Pip[0] <= n_pic_N1Pip;
+      pis_N1Pip[0] <= n_pis_N1Pip;
+      for (i = 0; i < 2; i = i + 1) begin
+            prc_N1Pip[i+1] <= prc_N1Pip[i];
+            prs_N1Pip[i+1] <= prs_N1Pip[i];
+            pic_N1Pip[i+1] <= pic_N1Pip[i];
+            pis_N1Pip[i+1] <= pis_N1Pip[i];
       end
+      for (i = 0; i <= NPip-2; i = i + 1) begin
+            p_info[i+1]   <= p_info[i];
+            p_switch[i+1] <= p_switch[i];
+      end
+
+      case(p_switch[N1])
+            2'b01: begin
+                  // it is assumed in this case, N1Pip[1] has p_a, and N1Pip[0] has p_b. Generate p'_a.
+                  // adder 1 will performe, pp_ar = cosb * p_ar - sinb * p_bi
+                  // adder 2 will performe, pp_ai = cosb * p_ai + sinb * p_br
+                  add1_a <= prc_N1Pip[1];
+                  add1_b <= pis_N1Pip[0] ^ SIGN_MASK;
+                  add2_a <= pic_N1Pip[1];
+                  add2_b <= prs_N1Pip[0];
+            end
+            2'b10: begin
+                  // it is assumed in this case, N1Pip[2] has p_a, and N1Pip[1] has p_b. Generate p'_b.
+                  // adder 1 will performe, pp_br = - sinb * p_ai + cosb * p_br
+                  // adder 2 will performe, pp_bi = sinb * p_ar + cosb * p_bi
+                  add1_a <= prc_N1Pip[1];
+                  add1_b <= pis_N1Pip[2] ^ SIGN_MASK;
+                  add2_a <= pic_N1Pip[1];
+                  add2_b <= prs_N1Pip[2];
+            end
+            default: begin
+                  // this is for cost function operator
+                  // adder 1 will performe, pp_ar = - sinb* p_ai + cosb *p_ar
+                  // adder 2 will performe, pp_ai = sinb *p_ar + cosb *p_ai
+                  add1_a <= prc_N1Pip[1];
+                  add1_b <= pis_N1Pip[1] ^ SIGN_MASK;
+                  add2_a <= pic_N1Pip[1];
+                  add2_b <= prs_N1Pip[1];
+            end
+      endcase
+      pr_N3Pip <= add1_res;
+      pi_N3Pip <= add2_res;
 end
 endmodule
