32ee37e8a09756d90b21710ce68f28ab50d12dcc
N1259534 <N1259534@my.ntu.ac.uk>
2026-05-05 21:57:34 +0100

    improve cost layer
    in this changes I added FIxed-point multiplier IP  module and test, improve gen-cost layer and created slicer module,

--- main/Base/Simulators/FPGA/NTU_FPGA2/gen_cost.sv
+++ main/Base/Simulators/FPGA/NTU_FPGA2/gen_cost.sv
@@ -1,6 +1,5 @@
 `timescale 1ns / 1ns
-// pipelined ALU test code for 2 floating points.
-// Hiroki Shibata, Tokyo Metropollitan University, created at Nottingham Trent University.
+// Amir Alizadeh - Hiroki Shibata, NTU and Tokyo Metropollitan University, created at Nottingham Trent University.
 
 // gamma should be in [-pi, pi]. 
 module gen_cost
@@ -8,107 +7,86 @@ module gen_cost
   parameter P=64, // number of word width
   parameter Ni=32) // width of additional information
   (
-   input  CLK,
+   input  CLK, // all ports
    input  RST,
    input  [P-1:0]  gamma, // cos gamma
    input  [P-1:0]   H,
-   output  [P-1:0]   Hr_o,
-   output  [P-1:0]   Hi_o,
-   input  [Ni-1:0]    info_in, // information, like addresses, enabled signal, and so on.
-   output  [Ni-1:0]    info_out // information, like addresses, enabled signal, and so on.
+   output signed [P-1:0]  Hr_o, // real part of e^(i gamma H)
+   output signed [P-1:0]  Hi_o // Img part of e^(i gamma H)
 );
-localparam N0 = 20; // latency mulFPF64
-localparam N1 = 11; // latency FP64 to Fix 56.53(sign)
-localparam N2 = 172; // latency of CORDIC. 56.53(sign) to 55.53(sign)
-localparam N3 = 15; // latency Fix 55.53(sign) to FP64
-localparam NPip = 1 + N0 + 1 + N1 + 1 + N2 + 1 + N3 + 1; // = 218, number of pipeline. Depends on IP core like addFPF64 used in this module.
 
-wire [P-1:0] n0_out;
-reg [63:0] n1_in;
-wire [55:0] n1_out;
-reg [55:0] n2_in;
-wire [54:0] n2_sin_out;
-wire [54:0] n2_cos_out;
-wire [55:0] cordic_in;
-reg [54:0] n3_sin_in;
-reg [54:0] n3_cos_in;
-wire [63:0] n3_sin_out;
-wire [63:0] n3_cos_out;
+localparam N0 = 12; // latency new test_mul 12 cycles
+localparam N1 = 172; // latency of CORDIC. 56.53(sign) to 55.53(sign)
 
-reg [P-1:0] Hi_NPip;
-reg [P-1:0] Hr_NPip;
-reg [P-1:0] H_0Pip;
-reg [P-1:0] gamma_0Pip;
-assign Hr_o = Hr_NPip;
-assign Hi_o = Hi_NPip;
-reg [Ni-1:0] p_info [NPip-1:0];
-assign info_out = p_info[NPip-1];
+reg signed [P-1:0] mul_in1;
+reg signed [P-1:0] mul_in2;
+reg signed [2*P-1:0] slcier_in;
+reg signed [P-1:0] cordic_in;
 
-mulFPF64 mulFPF_rc(
-      .clk(CLK),    //    clk.clk
-      .areset(RST), // areset.reset
-      .a(H_0Pip),      //    a.a, FP64
-      .b(gamma_0Pip),  //    b.b, FP64
-      .q(n0_out)       //    q.q, FP64
+
+reg signed [P-1:0]  cordic_c_out;
+reg signed [P-1:0]  cordic_s_out;
+
+// output of the IPs
+wire signed [P-2:0] cordic_cos_out;
+wire signed [P-2:0] cordic_sin_out;
+wire signed [2*P-1:0] mul_out; // output of multiplier = gamma*H
+wire signed [P-1:0]  slicer_out; // slicer output to cordic input
+
+
+
+test_mul new_mul(
+      .CLK(CLK),
+      .RST(RST),
+      .a(mul_in1), // H input of multiplier
+      .b(mul_in2),
+      .q(mul_out)
 );
-FP2Fix53 inst_FP2Fix53(
-		.clk(CLK),    // clk.clk input  wire        
-		.areset(RST), // areset.reset input  wire        
-		.a(n1_in),    // a.a input  wire [63:0] 
-		.q(n1_out)    // q.q output wire [55:0] 
+
+mul_slice slicer #(
+	.P(P),
+      .IA(4),
+      .IB(4),
+      .IOUT(4)
+      ) (
+      .CLK(CLK),
+      .RST(RST),
+      .a(mul_out),   // output of multiplier
+      .q(slicer_out)   // input of cordic
+      );
+		
+
+CORDIC_64_fixedP inst_cordic(
+      	.a(cordic_in),  //  input (it is H*gamma) wire width = [63:0] 
+		.areset(RST),    // areset.reset        
+		.c(cordic_cos_out),         // c.c output wire [62:0] , cosine
+		.clk(CLK),       // clk.clk         
+		.s(cordic_sin_out)          // s.s     output wire [62:0] , sine
 	);
 
-cordicR inst_cordic(
-		.a(n2_in),  // a.a input  wire [55:0] 
-		.areset(RST),    // areset.reset input  wire        
-		.c(n2_cos_out),         // c.c output wire [54:0] , cosine
-		.clk(CLK),       // clk.clk   input  wire        
-		.s(n2_sin_out)          // s.s     output wire [54:0] , sine
-	);
 
-Fix53toFP64 inst_Fix53toFP64(
-		.clk(CLK),    // clk.clk input  wire       
-		.areset(RST), // areset.reset input  wire        
-		.a(n3_sin_in),    //      a.a   input  wire [54:0] 
-		.q(n3_sin_out)    //      q.q output wire [63:0] 
-	);
-Fix53toFP64 inst2_Fix53toFP64(
-		.clk(CLK),    // clk.clk input  wire       
-		.areset(RST), // areset.reset input  wire        
-		.a(n3_cos_in),    //      a.a   input  wire [54:0] 
-		.q(n3_cos_out)    //      q.q output wire [63:0] 
-	);
 
-reg [31:0] CP; // program counter
-integer i;
 always @(posedge CLK) begin
+      
+      integer i;
 	if (RST) begin
-            for (i = 0; i < NPip; i = i + 1) begin
-                  p_info[i] <= 0;
-            end
-            Hi_NPip <= '0;
-            Hr_NPip <= '0;
-            H_0Pip <= '0;
-            gamma_0Pip <= '0;
-            n3_cos_in <= '0;
-            n3_sin_in <= '0;
-            n1_in <= '0;
-            n2_in <= '0;
-	end
+            mul_in1 <= '0; // input of mutiplier
+            mul_in2 <= '0; // second input of multiplier
+            slicer_in <= '0; // input of slicer
+            cordic_in <= '0; // input of cordic
+				end
       else begin
-            n3_cos_in <= n2_cos_out;
-            n3_sin_in <= n2_sin_out;
-            n1_in <= n0_out;
-            n2_in <= n1_out;
-            gamma_0Pip <= gamma;
-            H_0Pip <= H;
-            Hi_NPip <= n3_sin_out;
-            Hr_NPip <= n3_cos_out;
-            p_info[0] <= info_in;
-            for (i = 0; i <= NPip-2; i = i + 1) begin
-                  p_info[i+1] <= p_info[i];
+            mul_in1 <= gamma;
+            mul_in2 <= H;	
             end
-      end
+            slicer_in <= mul_out; // input of slicer is the output 
+		cordic_in <= slicer_out; // input of cordic is the output of multiplier after N0 cycles.
+		cordic_c_out <= {{1{cordic_cos_out[P-2]}}, cordic_cos_out}; // sign extend to 64 bit
+		cordic_s_out <= {{1{cordic_sin_out[P-2]}}, cordic_sin_out};
+
 end
+
+assign Hr_o = cordic_c_out; 
+assign Hi_o = cordic_s_out; 
 endmodule
 
