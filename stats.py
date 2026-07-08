8d663d71e003268e5379646a8d7406aacd924b3b
Hiroki Shibata <hshibata@tmu.ac.jp>
2026-07-09 00:03:56 +0100

    update

--- stats.py
+++ stats.py
@@ -4,7 +4,8 @@ import time
 
 # arguments
 NQ = 12
-for NQ in [4, 8, 10, 12, 14, 16]:
+# for NQ in [4, 8, 10, 12, 14, 16]:
+for NQ in [8, 10]:
     for NP in range(1, 33):
         args = ["python3", "-m", "main.Base.Simulators.FPGA.fpga_sim_test", "--sim", "cpp", "--NQ", str(NQ), "--port", "COM3", "--Np", str(NP)]
         print("running...", args)
